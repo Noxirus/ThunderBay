6ff1f7dd101bc508054bb4c91a5ab32fd3f9ecd1
unbeknownst6k9 <46736851+unbeknownst6k9@users.noreply.github.com>
2020-08-15 18:47:10 -0400

    Update projectile controller
    I added a scriptable object for projectile and projectile movement. I haven't tested it yet, but to test if it's working, simpily instantiate the GameObject should be fine

---
+++ Grove/Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+    public Projectile projectile;
+    public List<string> hitTargetTag;//the list of target that should hit on
+    public float speed;
+    public float projectileDuration;
+    public float muzzleEffectDuration;
+    public float hitEffectDuration;
+
+    private Rigidbody rig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject muzzle = Instantiate(projectile.muzzleEffect, transform.position, Quaternion.Euler(transform.forward));
+        Destroy(muzzle.gameObject, muzzleEffectDuration);
+        Destroy(gameObject, projectileDuration);
+        rig = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rig.velocity = transform.forward * speed;
+    }
+
+    //
+    private void OnTriggerEnter(Collider other)
+    {
+        //if it hits the correct object
+        if (isInTargetList(other.gameObject)) {
+            rig.velocity = Vector3.zero;//stop the projectile
+            //disable all child object which are the effects
+            for (int i = 0; i < transform.childCount; i++) {
+                transform.GetChild(i).gameObject.SetActive(false);
+            }
+            //do damage here
+
+            GameObject hit = Instantiate(projectile.hitEffect, transform.position, Quaternion.Euler(transform.forward));
+            Destroy(hit.gameObject, hitEffectDuration);
+            Destroy(gameObject);
+        }
+    }
+
+    //helper method to check if the hit object is in the target list
+    private bool isInTargetList(GameObject obj) {
+        int matches = 0;
+        for (int i = 0; i < hitTargetTag.Count; i++) {
+            if (obj.tag == hitTargetTag[i]) {
+                matches++;
+            }
+        }
+        return matches == 0;
+    }
+}
