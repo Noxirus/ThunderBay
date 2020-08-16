90ad75837e68cc1520665ea1aabd95f29bb0d4a8
unbeknownst6k9 <46736851+unbeknownst6k9@users.noreply.github.com>
2020-08-16 16:20:41 -0400

    Update fire slime minion

---
+++ Grove/Assets/Scripts/AnimatorController Scripts/FireSlimeAnimateController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FireSlimeAnimateController : CharacterAnimationController
+{
+    private NavMeshAgent agent;
+
+    public override void animateAttack()
+    {
+        animateController.SetTrigger("AttackTrigger");
+    }
+
+    public override void animateDie()
+    {
+        animateController.SetTrigger("DieTrigger");
+    }
+
+    public override void animateMove()
+    {
+        animateController.SetFloat("Speed", agent.velocity.magnitude);
+    }
+
+}
