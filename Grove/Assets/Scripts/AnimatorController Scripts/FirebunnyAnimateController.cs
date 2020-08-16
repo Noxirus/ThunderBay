16e14d2497814b9c0979057d87bb76f1d7a512e3
unbeknownst6k9 <46736851+unbeknownst6k9@users.noreply.github.com>
2020-08-16 15:52:04 -0400

    Update fire bunny animation, hit paralyzed phase, and die delay phase

---
+++ Grove/Assets/Scripts/AnimatorController Scripts/FirebunnyAnimateController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FirebunnyAnimateController : CharacterAnimationController
+{
+    private NavMeshAgent agent;
+    private bool Die = false;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        
+    }
+
+    public override void animateMove()
+    {
+        animateController.SetFloat("Speed", agent.velocity.magnitude);
+    }
+
+    public override void animateAttack()
+    {
+        animateController.SetTrigger("AttackTrigger");
+    }
+
+    public override void animateDamage()
+    {
+        animateController.SetTrigger("DamageTrigger");
+    }
+
+    public override void animateDie()
+    {
+        Die = !Die;
+        animateController.SetTrigger("DieTrigger");
+    }
+}
