56eaba90ef16616552df8c03ba95a22bdbf34362
Hamish Harrison <57675214+Noxirus@users.noreply.github.com>
2020-08-16 15:19:41 -0600

    Fixed UI Added more to splash screen
    Fixed energy bar
    Added AGJ logo

--- Grove/Assets/Scripts/StartGame.cs
+++ Grove/Assets/Scripts/StartGame.cs
@@ -5,9 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
+    float splashTimer = 3f;
+    bool readyToPlay = false;
+    [SerializeField] GameObject aGJLogo = null;
+    [SerializeField] GameObject directionsSplash = null;
+
+    private void Start()
+    {
+        StartCoroutine(SplashTimer());
+    }
+
+    IEnumerator SplashTimer()
+    {
+        yield return new WaitForSeconds(splashTimer);
+        directionsSplash.SetActive(true);
+        aGJLogo.SetActive(false);
+        readyToPlay = true;
+    }
+
     void Update()
     {
-        if (Input.anyKey)
+        if (Input.anyKey && readyToPlay)
         {
             InitiateGame();
         }
--- Grove/Assets/Scripts/UserInterface.cs
+++ Grove/Assets/Scripts/UserInterface.cs
@@ -9,26 +9,45 @@ public class UserInterface : MonoBehaviour
     [SerializeField] TextMeshProUGUI gemText = null;
     [SerializeField] TextMeshProUGUI middleText = null;
     [SerializeField] GameObject[] currentHearts = null;
+    [SerializeField] GameObject extraHeart = null;
     [SerializeField] Slider energySlider = null;
+    float middleTextTimer = 4f;
+    bool extraLife = false;
 
-    public void MiddleTextMessage(string message)
+    public void MiddleTextMessage(string message, Color textColor)
     {
+        middleText.color = textColor;
         middleText.text = message;
         middleText.gameObject.SetActive(true);
+        StopAllCoroutines();
+        StartCoroutine(HideMiddleText());
     }
 
-    public void HideMiddleText()
+    IEnumerator HideMiddleText()
     {
+        yield return new WaitForSeconds(middleTextTimer);
         middleText.gameObject.SetActive(false);
     }
 
     public void UpdateHealth(int hearts)
     {
+        if(hearts == 4 && extraLife)
+        {
+            currentHearts[0].SetActive(true);
+            currentHearts[1].SetActive(true);
+            currentHearts[2].SetActive(true);
+            currentHearts[3].SetActive(true);
+        }
+        
         if(hearts == 3)
         {
             for(int i = 0; i < currentHearts.Length; i++)
             {
                 currentHearts[i].SetActive(true);
+            }
+            if (extraLife)
+            {
+                currentHearts[3].SetActive(false);
             }
         }
         if(hearts == 2)
@@ -36,19 +55,37 @@ public class UserInterface : MonoBehaviour
             currentHearts[0].SetActive(true);
             currentHearts[1].SetActive(true);
             currentHearts[2].SetActive(false);
+            if (extraLife)
+            {
+                currentHearts[3].SetActive(false);
+            }
         }        
         if(hearts == 1)
         {
             currentHearts[0].SetActive(true);
             currentHearts[1].SetActive(false);
             currentHearts[2].SetActive(false);
+            if (extraLife)
+            {
+                currentHearts[3].SetActive(false);
+            }
         }        
         if(hearts == 0)
         {
             currentHearts[0].SetActive(false);
             currentHearts[1].SetActive(false);
             currentHearts[2].SetActive(false);
+            if (extraLife)
+            {
+                currentHearts[3].SetActive(false);
+            }
         }
+    }
+
+    public void ExtraLife()
+    {
+        extraHeart.SetActive(true);
+        extraLife = true;
     }
 
     public void UpdateEnergy(float energy)
